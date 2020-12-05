3b2b3407323e190c84e7c757511949a0c4a93022
StravopodisNikos <stravnik.rob@gmail.com>
2020-12-05 15:00:45 +0200

    config files sync for test

--- motorIDs.h
+++ motorIDs.h
@@ -6,8 +6,5 @@
 #define DXL2_ID                         2                   // Dynamixel#2 ID: 2
 #define DXL3_ID                         3                   // Dynamixel#3 ID: 3
 
-// Passive Joints
-#define PSEUDO_STP1_ID                  1                   // PseudoJoint1 axis Stepper Nema23
-#define PSEUDO_STP2_ID                  2                   // PseudoJoint2 axis Stepper Nema23
 
 
